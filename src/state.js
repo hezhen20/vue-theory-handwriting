ebbc114a5f0d936947c14c94b550a4af27025547
hezhen <hezhen20@foxmail.com>
2022-10-27 11:14:41 +0800

    vue 属性代理

--- src/state.js
+++ src/state.js
@@ -21,6 +21,16 @@ export function initSate(vm) {
 
 function initProps() {}
 function initMethods() {}
+function proxy(vm, data, key) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[data][key]  // vm.a 就相当于 vm._data.a
+    },
+    set(newValue) {
+      vm[data][key] = newValue    // vm.a = 10 就相当于 vm._data.a = 10
+    }
+  })
+}
 function initData(vm) {
   let data = vm.$options.data
 
@@ -28,6 +38,10 @@ function initData(vm) {
   // 添加 _data 使得 用户可以通过实例访问
   vm._data = data = typeof data == 'function' ? data.call(vm) : data
   
+  for (const key in data) {
+    proxy(vm, '_data', key)
+  }
+
   observe(data)
 }
 function initComputed() {}
