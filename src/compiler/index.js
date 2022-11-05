ebe41f9ab73502648722321418520bac1f6396b3
hezhen <hezhen20@foxmail.com>
2022-11-05 11:06:35 +0800

    将字符串代码变成render函数

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -10,4 +10,10 @@ export function compileToFunctions(template) {
 
   // 3. 通过 ast 树，生成代码
   let code = generate(ast)
+
+  // 4. 将字符串变成函数
+  // 通过 with 指定作用域，这样在 code 中的变量就能拿到对应组件实例的变量了
+  let render = new Function(`with(this){return ${code}}`)
+
+  return render
 }
